edb81a021521ad346e83b2020ecca721b9ae24d9
guillaumeautret <guillaume.autret@gmail.com>
2024-11-11 11:16:01 +0100

    ok mais sans style2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,5 +5,16 @@ class QuestionsController < ApplicationController
   def answer
     @question = params[:question]
     @answer = 'hello'
+
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    # Si le message est I am going to work, le coach répondra Great!.
+    # Si le message contient un point d’interrogation ? à la fin, le coach répondra Silly question, get dressed and go to work!.
+    # Sinon, le coach répondra I don't care, get dressed and go to work!.
   end
 end
